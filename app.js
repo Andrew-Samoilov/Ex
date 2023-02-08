3eb5b9231073add3d1ffe079b1a599c97a72e7a0
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-02-08 11:29:36 +0200

    +

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.get("/about", function (request, response) {
 app.get("/contact", function (request, response) {
     response.send("<h1>Контакты</h1>");
 });
-// app.listen(3000);
 
 app.listen(port, host, function () {
     console.log(`Server listens http://${host}:${port}`)
